e3a79a408b2474a8645822f2350c6e25b283c4a0
Cameron West <camonater62@gmail.com>
2023-04-17 10:50:48 -0700

    git init

---
+++ Source/SpaceShark/SpaceSharkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceSharkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACESHARK_API ASpaceSharkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
